ea8cde0f42ba8934f47336d649dff6f4e6bb2b8a
webpacs <webpacs.co@gmail.com>
2020-08-05 03:42:31 -0300

    Do relative imports for files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:clean_dart_github_search_mobx/app/app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
+import 'app/app_module.dart';
 
 void main() => runApp(ModularApp(module: AppModule()));
