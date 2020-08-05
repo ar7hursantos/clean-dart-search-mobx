64b54e761a1decf757a0ee60c9c3a1d71c305397
webpacs <webpacs.co@gmail.com>
2020-08-05 20:13:33 -0300

    code extraction for a new method

--- lib/app/search/presenter/search_page.dart
+++ lib/app/search/presenter/search_page.dart
@@ -44,49 +44,53 @@ class _SearchPageState extends ModularState<SearchPage, SearchStore> {
     }
   }
 
+  Widget _buildBody() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(top: 8, right: 8, left: 8),
+          child: TextField(
+            onChanged: controller.setSearchText,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Pesquise...",
+            ),
+          ),
+        ),
+        Expanded(
+          child: Observer(builder: (_) {
+            var state = controller.state;
+
+            if (state is ErrorState) {
+              return _buildError(state.error);
+            }
+
+            if (state is StartState) {
+              return Center(
+                child: Text('Digite alguma coisa...'),
+              );
+            } else if (state is LoadingState) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is SuccessState) {
+              return _buildList(state.list);
+            } else {
+              return Container();
+            }
+          }),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Github Search"),
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 8, right: 8, left: 8),
-            child: TextField(
-              onChanged: controller.setSearchText,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Pesquise...",
-              ),
-            ),
-          ),
-          Expanded(
-            child: Observer(builder: (_) {
-              var state = controller.state;
-
-              if (state is ErrorState) {
-                return _buildError(state.error);
-              }
-
-              if (state is StartState) {
-                return Center(
-                  child: Text('Digita alguma coisa...'),
-                );
-              } else if (state is LoadingState) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if (state is SuccessState) {
-                return _buildList(state.list);
-              } else {
-                return Container();
-              }
-            }),
-          )
-        ],
-      ),
+      body: _buildBody(),
     );
   }
 }
